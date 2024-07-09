d4693b8e089dc697302d1ff32cbaa3c2f029c7e2
Lavanya B R <lavanyaece450@gmail.com>
2024-07-09 14:44:46 +0530

    modified gitignore file and added constants.swift file

---
+++ youtube-onedaybuild/ConstantsTemplate.swift
@@ -0,0 +1,17 @@
+//
+//  ConstantsTemplate.swift
+//  youtube-onedaybuild
+//
+//  Created by Lavanya B R on 09/07/24.
+//
+
+import Foundation
+
+// add your keys and uncomment the below code
+/*
+ struct Constants {
+ static var API_KEY = ""
+ static var PLAYLIST_ID = ""
+ static var API_URL = "https://youtube.googleapis.com/youtube/v3/playlistItems?part=snippet&playlistId=\(Constants.PLAYLIST_ID)&key=\(Constants.API_KEY)"
+ }
+ */
