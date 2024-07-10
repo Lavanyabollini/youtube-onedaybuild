b849d9120797756313955aadf92c4bd97d7b56a7
Lavanya B R <lavanyaece450@gmail.com>
2024-07-10 11:32:18 +0530

    added tableview, custom protocol in the model, displaying video titles in the tableview

--- youtube-onedaybuild/Model.swift
+++ youtube-onedaybuild/Model.swift
@@ -6,8 +6,15 @@
 //
 
 import Foundation
+
+protocol ModelDelegate{
+    func videosFetched (_ videos: [Video])
+}
+
 class Model {
-    func getVideos(){
+    
+    var delegate:ModelDelegate?
+    func getVideos() {
         // create an URL object
         let url = URL(string: Constants.API_URL)
         guard url != nil  else {
@@ -26,6 +33,10 @@ class Model {
                 let decoder = JSONDecoder()
                 decoder.dateDecodingStrategy = .iso8601
                 let response = try decoder.decode(Response.self, from: data!)
+                if let items = response.items {
+                    //  call the videos fetched method of the delegate
+                    self.delegate?.videosFetched(items)
+                }
                 dump(response)
             }
             catch {
--- youtube-onedaybuild/ViewController.swift
+++ youtube-onedaybuild/ViewController.swift
@@ -6,17 +6,51 @@
 //
 
 import UIKit
+// followed tutorial https://www.youtube.com/watch?v=lQx1hAOQkpc&list=PLMRqhzcHGw1aLoz4pM_Mg2TewmJcMg9ua&index=6
 
-class ViewController: UIViewController {
-
-    var model = Model()
+class ViewController: UIViewController,UITableViewDataSource, UITableViewDelegate,ModelDelegate {
     
+    var model = Model()
+    var videos = [Video]()
+    
+    @IBOutlet weak var tabelView: UITableView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        model.getVideos()
+        //set itself as delegate of the model
+        model.delegate = self
+       model.getVideos()
+        
+       
+    }
+// MARK: - Model delegate methods
+    func videosFetched(_ videos: [Video]) {
+        // set the returned videos to our video property
+        self.videos = videos
+        // refresh the tableview
+        DispatchQueue.main.async {
+            self.tabelView.reloadData()
+        }
     }
 
-
+    // MARK: - TableView datasource and delegate methods
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return videos.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: Constants.VIDEOCELL_ID, for: indexPath)
+        //configure cell with data
+        let title = videos[indexPath.row].title
+        cell.textLabel?.text = title
+        cell.textLabel?.numberOfLines = 0
+        return cell
+        
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+    }
 }
 
